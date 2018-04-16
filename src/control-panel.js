b154c4f7b95bd84360ea3cb5261dd3cca2a1393c
vivek malva <vmalva@sapient.com>
2018-04-17 01:09:45 +0530

    datatable v1.10.16

--- src/control-panel.js
+++ src/control-panel.js
@@ -23,7 +23,11 @@ $(function () {
 
     }
     const render = (state) => {
-        
+        $('#countTable_first').text('a');
+        // $('.first').text('').appendTo('<i class="fa fa-angle-double-left" aria-hidden="true"></i>');
+        $('.previous').text('').appendTo('<i class="fa fa-angle-left" aria-hidden="true"></i>');
+        $('.next').text('').appendTo('<i class="fa fa-angle-right" aria-hidden="true"></i>');
+        $('.last').text('').appendTo('<i class="fa fa-angle-double-right" aria-hidden="true"></i>');
         $("#countTable").dataTable({
             // "bProcessing": true,
             // "bServerSide": true,
@@ -37,7 +41,7 @@ $(function () {
             // "sPaginationType": "full_numbers",
             // "pageLength": 5
             searching: false,
-            // ordering: false,
+            ordering: false,
             info: false,
             paging: true,
             lengthChange: false,
@@ -70,11 +74,7 @@ $(function () {
                  } 
             ]
         });
-
-        $('.first').text('').appendTo('<i class="fa fa-angle-double-left" aria-hidden="true"></i>');
-        $('.previous').text('').appendTo('<i class="fa fa-angle-left" aria-hidden="true"></i>');
-        $('.next').text('').appendTo('<i class="fa fa-angle-right" aria-hidden="true"></i>');
-        $('.last').text('').appendTo('<i class="fa fa-angle-double-right" aria-hidden="true"></i>');
+       
 
 
         
